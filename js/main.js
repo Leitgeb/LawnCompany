16ab34816392a7afd9b3cd1036047c41f0da04fd
Richard Leitgeb <richard.leitgeb@outlook.com>
2016-06-29 21:24:00 -0500

    Create services section and footer.

--- js/main.js
+++ js/main.js
@@ -12,5 +12,30 @@ function scrollEvent(element, style) {
     });
 }
 
+//Smooth scroll navigation 
+$(document).ready(function(){
+  function NavScroll(elem) {
+      $(elem).on('click', function(event) {
+
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+        window.location.hash = hash;
+        });
+    });
+  }
+  
+  NavScroll(".main-nav a");
+  NavScroll(".contactScroll");  
+  NavScroll(".main-footer a");
+  
+}); //End smooth scroll navigation
+
+  
 scrollEvent('.main-nav', 'nav-scroll-style');
 scrollEvent('.phone', 'phone-scroll-style');
